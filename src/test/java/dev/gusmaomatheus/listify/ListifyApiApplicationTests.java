c2a9656d90538a6120614783c5e376baa7b2c3d7
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-06-26 22:51:39 -0300

    feat: create project base
    This commit creates the initial files for creating the project.

---
+++ src/test/java/dev/gusmaomatheus/listify/ListifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gusmaomatheus.listify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
