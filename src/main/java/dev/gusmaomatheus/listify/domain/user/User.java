29c8ad41acaeaf0ac11103dbde3ef87754192a5a
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-06-27 21:15:45 -0300

    feat: create 'user' entity
    This commit creates a class to represent an entity from the 'users'
    table in the database.

---
+++ src/main/java/dev/gusmaomatheus/listify/domain/user/User.java
@@ -0,0 +1,25 @@
+package dev.gusmaomatheus.listify.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    private String email;
+    private String password;
+}
