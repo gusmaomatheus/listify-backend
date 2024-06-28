67fd49cc92b20f9a16865572b1d2655fbb77e77f
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-06-27 23:45:33 -0300

    feat: create SecurityConfig
    This commit creates a class to configure application security settings.

---
+++ src/main/java/dev/gusmaomatheus/listify/infra/security/SecurityConfig.java
@@ -0,0 +1,49 @@
+package dev.gusmaomatheus.listify.infra.security;
+
+import dev.gusmaomatheus.listify.service.auth.AuthServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final SecurityFilter filter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
+        return security
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers(HttpMethod.POST, "listify/api/auth/login").permitAll()
+                                .requestMatchers(HttpMethod.GET, "listify/api/auth/logout").permitAll()
+                                .requestMatchers(HttpMethod.POST, "listify/api/users/register").permitAll()
+                                .anyRequest().authenticated()
+                ).addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+}
