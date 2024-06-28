6ccae54fa84457668c904460e0bd0ba3c7b140e5
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-06-27 22:14:30 -0300

    chore: add @Repository annotation to UserRepository
    This commit add @Repository annotation to UserRepository to enable
    component scanning and bean creation by Spring.

--- src/main/java/dev/gusmaomatheus/listify/repository/UserRepository.java
+++ src/main/java/dev/gusmaomatheus/listify/repository/UserRepository.java
@@ -2,6 +2,8 @@ package dev.gusmaomatheus.listify.repository;
 
 import dev.gusmaomatheus.listify.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
