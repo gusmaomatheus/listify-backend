ccbfa2893f855762d2c16913c12ccf3b3df9c98b
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-06-27 22:05:46 -0300

    refactor: rename email field to username in User and your dtos
    This commit rename the email field to username in both User entity and
    RegisterRequestDTO transfer-object to match the column name in the
    database. This change ensures consistency between the code and the
    database schema.

--- src/main/java/dev/gusmaomatheus/listify/domain/user/User.java
+++ src/main/java/dev/gusmaomatheus/listify/domain/user/User.java
@@ -20,6 +20,6 @@ public class User {
     private String firstName;
     @Column(name = "last_name")
     private String lastName;
-    private String email;
+    private String username;
     private String password;
 }
