f82233c680802b77ce5cef60eff0945ae46891c8
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-06-27 23:06:04 -0300

    feat: implements authentication logic
    This commit implements authentication logic to SecurityFilter.

--- src/main/java/dev/gusmaomatheus/listify/infra/security/SecurityFilter.java
+++ src/main/java/dev/gusmaomatheus/listify/infra/security/SecurityFilter.java
@@ -1,19 +1,58 @@
 package dev.gusmaomatheus.listify.infra.security;
 
+import dev.gusmaomatheus.listify.domain.user.User;
+import dev.gusmaomatheus.listify.infra.token.TokenService;
+import dev.gusmaomatheus.listify.repository.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Component
+@RequiredArgsConstructor
 public class SecurityFilter extends OncePerRequestFilter {
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = recoverToken(request);
+        String username = tokenService.validateToken(token);
 
+        if (!Objects.isNull(username)) {
+            Optional<User> user = userRepository.findByUsername(username);
+
+            if (user.isEmpty()) {
+                throw new UsernameNotFoundException(String.format("No records found for username %s.", username));
+            }
+
+            List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, authorities);
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (Objects.isNull(authorizationHeader)) return null;
+
+        return authorizationHeader.replace("Bearer ", "");
     }
 }
