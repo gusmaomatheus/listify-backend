371e39e0e1f4c46eeecc7158699149ad680dbeb6
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-07-08 20:28:25 -0300

    feat: add UserServiceImpl#register
    This commit implements the logic for the method that creates a new user.

--- src/main/java/dev/gusmaomatheus/listify/service/user/UserServiceImpl.java
+++ src/main/java/dev/gusmaomatheus/listify/service/user/UserServiceImpl.java
@@ -1,11 +1,31 @@
 package dev.gusmaomatheus.listify.service.user;
 
+import dev.gusmaomatheus.listify.domain.user.User;
+import dev.gusmaomatheus.listify.dto.auth.RegisterRequestDTO;
+import dev.gusmaomatheus.listify.infra.exception.exceptions.UsernameAlreadyRegisteredException;
+import dev.gusmaomatheus.listify.infra.security.SecurityConfig;
 import dev.gusmaomatheus.listify.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
     private final UserRepository repository;
+    private final ModelMapper modelMapper;
+    private final SecurityConfig securityConfig;
+
+    @Override
+    public void register(RegisterRequestDTO request) {
+        if (repository.findByUsername(request.username()).isPresent()) {
+            throw new UsernameAlreadyRegisteredException("There is already a registered user using this username.");
+        }
+
+        User user = modelMapper.map(request, User.class);
+
+        user.setPassword(securityConfig.passwordEncoder().encode(request.password()));
+
+        repository.save(user);
+    }
 }
