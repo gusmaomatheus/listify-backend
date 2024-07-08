cb76dda8b67ddfc895b4f5544322f57ff80be2e9
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-07-08 18:31:37 -0300

    feat: crate CustomExceptionHandler
    This commit creates a class to handling an api custom exceptions.

---
+++ src/main/java/dev/gusmaomatheus/listify/infra/exception/CustomExceptionHandler.java
@@ -0,0 +1,8 @@
+package dev.gusmaomatheus.listify.infra.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+}
