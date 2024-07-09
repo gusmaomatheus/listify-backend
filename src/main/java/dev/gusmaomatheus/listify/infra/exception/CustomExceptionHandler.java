7da0ed9a40d6afe278e552dae7db3fb1ba553983
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-07-09 00:24:05 -0300

    feat: add exception handler for UsernameAlreadyRegisteredException
    Creates CustomExceptionHandler#usernameAlreadyRegistered to handle
    UsernameAlreadyRegisteredException and return a 400 status with a
    detailed error response.

--- src/main/java/dev/gusmaomatheus/listify/infra/exception/CustomExceptionHandler.java
+++ src/main/java/dev/gusmaomatheus/listify/infra/exception/CustomExceptionHandler.java
@@ -1,8 +1,26 @@
 package dev.gusmaomatheus.listify.infra.exception;
 
+import dev.gusmaomatheus.listify.infra.exception.custom.UsernameAlreadyRegisteredException;
+import dev.gusmaomatheus.listify.infra.exception.response.ExceptionResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(UsernameAlreadyRegisteredException.class)
+    private ResponseEntity<ExceptionResponse> usernameAlreadyRegistered(UsernameAlreadyRegisteredException exception) {
+        return ResponseEntity.status(400).body(
+                new ExceptionResponse(
+                        400,
+                        exception.getMessage(),
+                        "The username you tried to register is already in use by someone else in our system.",
+                        LocalDateTime.now()
+                )
+        );
+    }
 }
