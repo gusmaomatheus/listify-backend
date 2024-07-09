f3985e74436f8637a7ff780eecf51ed5366897eb
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-07-09 00:09:02 -0300

    feat: add UserController#register
    This commit creates an endpoint to handling for creates a user requests.

--- src/main/java/dev/gusmaomatheus/listify/controller/UserController.java
+++ src/main/java/dev/gusmaomatheus/listify/controller/UserController.java
@@ -1,13 +1,25 @@
 package dev.gusmaomatheus.listify.controller;
 
+import dev.gusmaomatheus.listify.dto.auth.AuthResponseDTO;
+import dev.gusmaomatheus.listify.dto.auth.RegisterRequestDTO;
 import dev.gusmaomatheus.listify.service.user.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class UserController {
     private final UserServiceImpl service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponseDTO> register(@RequestBody RegisterRequestDTO request) {
+        service.register(request);
+
+        return ResponseEntity.status(201).body(new AuthResponseDTO(request.username(), ""));
+    }
 }
