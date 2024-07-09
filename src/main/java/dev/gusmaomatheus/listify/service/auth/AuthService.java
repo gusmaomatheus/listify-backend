d97df82e14b2ed58c2b1746306aaf6a504629025
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-07-09 00:29:09 -0300

    feat: create AuthService#login
    This commit creates AuthService#login method to auth a user.

--- src/main/java/dev/gusmaomatheus/listify/service/auth/AuthService.java
+++ src/main/java/dev/gusmaomatheus/listify/service/auth/AuthService.java
@@ -1,6 +1,8 @@
 package dev.gusmaomatheus.listify.service.auth;
 
+import dev.gusmaomatheus.listify.dto.auth.LoginRequestDTO;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface AuthService extends UserDetailsService {
+    String login(LoginRequestDTO request);
 }
