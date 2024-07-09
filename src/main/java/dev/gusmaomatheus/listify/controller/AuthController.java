b2e2d2ba51cee6fdc6e6e3e4802db4f15ccd67fe
Matheus Gusmão <mgusmao.dev@gmail.com>
2024-07-09 00:53:56 -0300

    feat: add AuthController#login
    This commit creates an endpoint to handling for authenticate a user.

--- src/main/java/dev/gusmaomatheus/listify/controller/AuthController.java
+++ src/main/java/dev/gusmaomatheus/listify/controller/AuthController.java
@@ -1,7 +1,12 @@
 package dev.gusmaomatheus.listify.controller;
 
+import dev.gusmaomatheus.listify.dto.auth.AuthResponseDTO;
+import dev.gusmaomatheus.listify.dto.auth.LoginRequestDTO;
 import dev.gusmaomatheus.listify.service.auth.AuthServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthServiceImpl service;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginRequestDTO request) throws Exception {
+        String token = service.login(request);
+
+        return ResponseEntity.status(200).body(
+                new AuthResponseDTO(
+                        request.username(),
+                        token
+                )
+        );
+    }
 }
